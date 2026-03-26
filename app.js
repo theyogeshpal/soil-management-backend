c8b54ced9ee981a59d391e057aa86f24da5bb475
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-03-26 21:44:11 +0530

    frontend url

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(async (req, res, next) => {
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CLIENT_URL || ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175","https://soil-management-backend.onrender.com"],
+  origin: process.env.CLIENT_URL || ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175","https://soil-management.vercel.app/login"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
